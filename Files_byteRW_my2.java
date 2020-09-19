07b23a2f31269109701db6abf66d25beb074d9e7
Chheme <chemetitova2001@gmail.com>
2020-09-20 02:14:00 +0900

    Примеры 5 лабораторной работы

---
+++ Files_byteRW_my2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Files_byteRW_my2 {
+    Scanner sc=new Scanner(System.in,"cp1251");
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Введите имя файла => ");
+        String fname=sc.nextLine();
+        try{
+            File f1=new File(fname);
+            f1.createNewFile(); // файл создан
+            System.out.println("Полный путь файла: "+ f1.getAbsolutePath());
+
+            System.out.print("Введите количество строк для записи в файл => ");
+            int n=sc.nextInt();
+            DataOutputStream dOut=
+                    new DataOutputStream( new FileOutputStream(f1));
+            sc.nextLine(); //очистка буфера
+            for (int i = 0; i < n; i++) {
+                System.out.print("Введите строку для записи в файл => ");
+                String s=sc.nextLine();
+                dOut.writeUTF(s );
+                //или dOut.writeUTF(s +"\n" ); – запись отдельных строк
+            }
+            dOut.flush(); // дописываем несохраненные данные на диск
+            dOut.close(); // закрываем поток
+
+
+// Чтение и вывод данных из созданного файла
+            DataInputStream dIn=new DataInputStream(new FileInputStream(f1));
+            while (true) {
+                System.out.println(dIn.readUTF());
+            }
+        }catch (Exception e) {
+            System.out.println(""+e);
+        }
+    }
+}
+
+
+
---
+++ Primer1.java
@@ -0,0 +1,40 @@
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+
+public class Primer1 {
+    public static void readAllByByte(InputStream in) throws IOException {
+        while (true) {
+            int oneByte = in.read(); // читает 1 байт
+            if (oneByte != -1) { // признак отсутствия конца файла
+                System.out.print((char) oneByte);
+            } else {
+                System.out.print("\n" + "end");
+                break;
+            }
+        } }
+        public static void main(String[] args) throws IOException{
+            try {
+                InputStream inFile = new FileInputStream("c:/tmp/text.txt");
+                readAllByByte(inFile);
+                System.out.print("\n\n\n");
+                inFile.close();
+                InputStream inUrl = new URL("http://google.com").openStream();
+                readAllByByte(inUrl);
+                System.out.print("\n\n\n");
+                inUrl.close();
+                InputStream inArray=new ByteArrayInputStream(new byte [] {7, 9, 3,7,4});
+                readAllByByte(inArray);
+                System.out.print("\n\n\n");
+                inArray.close();
+
+            } catch (IOException e) {
+                System.out.println("Ошибка: "+ e);
+            }
+        }
+    }
+
+
